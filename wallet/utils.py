2ee33978b19a6ec872d7c74bd047905051808b3f
zarantem <89732957+zarantem@users.noreply.github.com>
2024-05-20 14:21:54 +0300

    Update utils.py

--- wallet/utils.py
+++ wallet/utils.py
@@ -1,8 +1,9 @@
 import jwt
+from .settings import JWT
 
 def generate_custom_jwt_token(user):
     payload = {
         'user_id': user.id,
         # Другие данные, которые вы хотите включить в токен
     }
-    return jwt.encode(payload, 'your-secret-key', algorithm='HS256')
+    return jwt.encode(payload, JWT, algorithm='HS256')
